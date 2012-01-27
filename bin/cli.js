e7a351cd0d40b62b87de9fa1ffc68b99699ecd2b
Dav Glass <davglass@gmail.com>
2012-01-27 10:25:37 -0600

    Using cli module to allow port configuration

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,26 @@
 #!/usr/bin/env node
 
-var server = require('yui-coverage');
+var cli = require('cli'),
+    path = require('path'),
+    server = require('yui-coverage');
 
+cli.enable('help', 'version');
 
-server.start();
+var meta = require(path.join('../package.json'));
+
+cli.parse({
+    version: ['v', 'Print the version'],
+    port: ['p', 'The port number to assign: default 3000', 'number', 3000]
+});
+
+cli.main(function() {
+    var opts = this.options;
+
+    if (opts.version) {
+        console.log('v' + meta.version);
+        return;
+    }
+
+    server.start(opts);
+});
+
